17b580f6a4734be48d6e9699486c89e7d6d3daf9
kyungeunnijjang <catdoger22@icloud.com>
2026-06-26 16:52:00 +0900

    카메라 및 레코드 화면 UI 거의완성

--- app/src/main/java/com/yourssu/setlog_cloning_androidrookiet1/MainActivity.kt
+++ app/src/main/java/com/yourssu/setlog_cloning_androidrookiet1/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.yourssu.setlog_cloning_androidrookiet1.ui.theme.RecordScreen
 import com.yourssu.setlog_cloning_androidrookiet1.ui.theme.SetLogCloningAndroidRookieT1Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +13,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SetLogCloningAndroidRookieT1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                RecordScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SetLogCloningAndroidRookieT1Theme {
-        Greeting("Android")
     }
 }
